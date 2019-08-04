70a08374037939444a64d7ff85f5bee4ad671af7
znw333 <znw333@126.com>
2019-08-05 00:27:40 +0800

    其他页面完善

--- AdminLTE-iframe/js/framework.js
+++ AdminLTE-iframe/js/framework.js
@@ -1,16 +1,24 @@
-/******************外页控制*************************/
-
-
-/******************内页控制*************************/
+/*!
+ * Copyright
+ * 外页控制
+ * @author zhuangnanwei
+ * @version 2019-7-6
+ */
++function ($) {
+}(jQuery);
+/*!
+ * Copyright
+ * 内页控制
+ * @author zhuangnanwei
+ * @version 2019-7-6
+ */
 var defaultForm = 'dashboard.html';     //默认页
 var defaultOpenStyle = 1;               //1.iframe 2.弹窗
-
 //提示弹窗------------------
 function showMsg(msg) {
     if (msg)
         alert(msg);
 }
-
 //切换页面------------------
 /**
  * 使用Iframe 切换
@@ -18,28 +26,22 @@ function showMsg(msg) {
  */
 function iframeOpen(url) {
     if (url) {
-        var ifr = window.parent.document.getElementById('ifr');
-        if (ifr)
-            ifr.src = defaultForm;
-        else
-            showMsg('打开异常(查无iframe)');
+        var ifr = $('#ifr', window.parent.document);
+        if (ifr) ifr.attr('src', defaultForm);//回调父级菜单被选中 todo
+        else showMsg('打开异常(查无iframe)');
     }
 }
-
 /**
  * 使用layer 切换
  * @param {any} url
  */
 function layerOpen(url) {
     if (url) {
-        var ifr = window.parent.document.getElementById('ifr');
-        if (ifr)
-            ifr.src = defaultForm;
-        else
-            showMsg('打开异常(查无iframe)');
+        var ifr = $('#ifr', window.parent.document);
+        if (ifr) ifr.attr('src', defaultForm);//回调父级菜单被选中 todo
+        else showMsg('打开异常(查无iframe)');
     }
 }
-
 /**
  * 切换页面
  * @param {any} url
@@ -69,7 +71,16 @@ function openUrl(url, openStyle) {
         }
     }
 }
-
+//子页标题------------------
+/**
+ * 子页标题设置
+ * @param {any}
+ */
+function SetFormTitle() {
+    console.log('SetFormTitle------------');
+    var li = $('.main-sidebar .sidebar-menu li[class=active]', window.parent.document);
+    if (li) $('.box-header .box-title').html($(li.find('a')).html());
+}
 //按钮------------------
 /**
  * 关闭 点击
@@ -79,7 +90,8 @@ $('.frmBtnClose').click(function () {
     openUrl(defaultForm, 1);
 });
 
+
 //默认初始化设置-----------
 $(function () {
-
+    SetFormTitle();
 });
